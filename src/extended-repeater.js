1647b30721cf159c2624abf80ee005b0f66e93bf
Julia Zaripova <zaripovaur@gmail.com>
2020-03-10 00:52:12 +0400

    add: extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,14 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    options.separator = options.separator !== undefined ? options.separator : '+';
+    options.additionSeparator = options.additionSeparator !== undefined ? options.additionSeparator : '|';
+    let addStr;
+    if (options.additionRepeatTimes === undefined || options.addition === undefined) {
+        addStr = '';
+    } else {
+        addStr = Array(options.additionRepeatTimes).fill(options.addition+'').join(options.additionSeparator+'');
+    }
+    if (options.repeatTimes === undefined) {return str+options.addition;}
+    let strAddStr = str+addStr;
+    return Array(options.repeatTimes).fill(strAddStr).join(options.separator+'');
 };
   
