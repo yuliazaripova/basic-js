ff028823b1424bc7c5da889d5b7310c385d4ac81
Julia Zaripova <zaripovaur@gmail.com>
2020-03-17 13:34:04 +0400

    add simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,28 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+     return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.push('( '+value+' )');
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (position <= 0 || position > this.chain.length || typeof position != 'number'){
+      this.chain = [];
+      throw new Error();
+    }
+    this.chain.splice(position-1,1);
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let chain = this.chain.join('~~');
+    this.chain = [];
+    return chain;
   }
 };
 
